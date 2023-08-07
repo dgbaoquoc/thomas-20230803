2c9877258bfc6682e838693cf4245d8ca6523c87
Thomas <dgbaoquoc@gmail.com>
2023-08-07 12:34:29 +0700

    feat: :hammer: dummy text for description coin

--- src/app/(coins)/coins/[id]/page.tsx
+++ src/app/(coins)/coins/[id]/page.tsx
@@ -1,13 +1,24 @@
-import { getCoin, getCoinMarketChart, getCoinMartChartOhlc } from "@/app/_actions/coin";
+import {
+  getCoin,
+  getCoinMarketChart,
+  getCoinMartChartOhlc,
+} from "@/app/_actions/coin";
 import { Breadcrumbs } from "@/components/pagers/breadcrumbs";
 import { Shell } from "@/components/shells/shell";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import Container from "@/components/ui/container";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { type Metadata } from "next";
 import { notFound } from "next/navigation";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-import CoinHeader from "./_components/header";
+import { LoremIpsum, loremIpsum } from "lorem-ipsum";
 import { tabs } from "@/constants";
+import CoinHeader from "./_components/header";
 import OverviewContent from "./_components/overview-content";
 
 // TODO: change to dynamic
@@ -63,16 +74,14 @@ export default async function CoinPage({
         />
         <div className="flex flex-col gap-4 md:gap-8">
           <CoinHeader coin={coin} />
-          <Tabs defaultValue="overview"
-          >
-            <TabsList
-                      className="inline-flex h-9 items-center w-full justify-start rounded-none border-b bg-transparent p-0"
-            >
+          <Tabs defaultValue="overview">
+            <TabsList className="inline-flex h-9 items-center w-full justify-start rounded-none border-b bg-transparent p-0">
               {tabs.map((tab) => (
                 <TabsTrigger
-                key={tab.title} value={tab.value}
-                disabled={tab.disabled}
-                className="data-[state=active]:bg-gray-100 data-[state=active]:text-foreground data-[state=active]:font-semibold"
+                  key={tab.title}
+                  value={tab.value}
+                  disabled={tab.disabled}
+                  className="data-[state=active]:bg-gray-100 data-[state=active]:text-foreground data-[state=active]:font-semibold"
                 >
                   {tab.title}
                 </TabsTrigger>
@@ -86,7 +95,26 @@ export default async function CoinPage({
               />
             </TabsContent>
             <TabsContent value="market">Market</TabsContent>
-            <TabsContent value="about">About</TabsContent>
+            <TabsContent value="about">
+              <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>
+                    What is {coin.name} ({coin.symbol})?
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    {new LoremIpsum().generateParagraphs(1)}
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                  <AccordionTrigger>
+                    Who is founder of {coin.name}?
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    {new LoremIpsum().generateParagraphs(2)}
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </TabsContent>
           </Tabs>
         </div>
       </Container>
